de83c779da9485deb597d09d64fd8b114dc4549e
GONBE <mrgonbegonbe@gmail.com>
2018-10-13 15:57:08 +0900

    雛形

---
+++ database_conf.php
@@ -0,0 +1,6 @@
+<?php
+$DBSERVER = 'localhost';
+$DBNAME = 'board';
+$DBUSER = 'gonbe'; //作成したユーザー名
+$DBPASSWD = 'apple'; //作成したユーザーのパスワード
+$dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8';
