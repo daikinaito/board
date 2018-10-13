05c9a32a8de71480354fa6567bc94bf66a1ad5af
GONBE <mrgonbegonbe@gmail.com>
2018-10-13 16:16:48 +0900

    エラーログ2

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
 
 header('Content-Type: text/html; charset=UTF-8');
     if(isset($_POST['id']) and isset($_POST['password'])){
