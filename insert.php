3f6ac8c8c0d1c11c668a2ac3c693baf23320ed0a
GONBE <mrgonbegonbe@gmail.com>
2018-11-03 10:56:07 +0900

    miss

--- insert.php
+++ insert.php
@@ -6,8 +6,8 @@
       header('Location: false.html');
     }
     require_once 'database_conf.php';
-    
-    $sql = 'INSERT INTO `board` (name, comment, created) VALUES (:name, :name, :comment, NOW())';
+
+    $sql = 'INSERT INTO `board` (name, comment, created) VALUES (:name, :comment, NOW())';
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':name', $name, \PDO::PARAM_STR);
     $stmt->bindValue(':comment', $comment, \PDO::PARAM_STR);
