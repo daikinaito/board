e9bf50398523f67cedcdc796bf85c14945d81e53
GONBE <mrgonbegonbe@gmail.com>
2018-11-03 15:44:10 +0900

    header追加

--- insert.php
+++ insert.php
@@ -6,10 +6,11 @@
       header('Location: false.html');
     }
     require_once 'database_conf.php';
-    
+
     $pdo = new \PDO($dsn, $DBUSER, $DBPASSWD, array(\PDO::ATTR_EMULATE_PREPARES => false));
     $sql = 'INSERT INTO comments (comment) VALUES (:comment)';
     $stmt = $pdo->prepare($sql);
 //    $stmt->bindValue(':name', $name, \PDO::PARAM_STR);
     $stmt->bindValue(':comment', $comment, \PDO::PARAM_STR);
     $stmt->execute();
+    header('Location: input.html');
