f43c0b1cc47d312f6ba6e2d359b15496d0fd7a88
GONBE <mrgonbegonbe@gmail.com>
2018-11-03 14:22:25 +0900

    ";忘れ

--- database_conf.php
+++ database_conf.php
@@ -3,4 +3,4 @@ $DBSERVER = 'localhost';
 $DBNAME = 'board';
 $DBUSER = 'gonbe'; //作成したユーザー名
 $DBPASSWD = 'apple'; //作成したユーザーのパスワード
-$dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8';
+$dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8';";
