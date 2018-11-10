68ae06108aef80e35625e5378f48bc6462a3572e
GONBE <mrgonbegonbe@gmail.com>
2018-11-10 14:24:50 +0900

    登録画面追加

---
+++ board2/register.php
@@ -0,0 +1,13 @@
+<?php
+    require_once 'database_conf.php';
+
+    sql = 'INSERT INTO users (id, name, password) VALUES (?,?,?)';
+    $stmt = $pdo->prepare($sql);
+
+    $data[] = $_POST['id'];
+    $data[] = $_POST['name'];
+    $data[] = $_POST['password'];
+
+    $stmt->execute($data);
+
+    header('Location: index.html');
