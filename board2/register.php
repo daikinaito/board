22c0260f795c281ca5524d9b8ad0602882c0028f
GONBE <mrgonbegonbe@gmail.com>
2018-11-17 15:00:00 +0900

    if編集

--- board2/register.php
+++ board2/register.php
@@ -5,42 +5,47 @@ error_reporting(E_ALL);
 try {
     require_once 'database_conf.php';
 
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $password = $_POST['password'];
+    $errors = [];
+    if(isset($_POST['id']) && $_POST['id'] !== '' && is_numeric($_POST['id'])) {
+       $id = $_POST['id'];
+    } else {
+        $errors[] = 'idが不正';
+    }
 
-    if(isset($id) and isset($name) and isset($password)){
-        var_dump($id);
-        if (is_numeric($id)) {
-            $sql = 'SELECT * FROM users WHERE userId = :userId';
+    if(isset($_POST['name']) && $_POST['name'] !== '') {
+        $id = $_POST['name'];
+    } else {
+        $errors[] = 'nameが不正';
+    }
+
+    if(isset($_POST['password']) && $_POST['password'] !== '') {
+        $id = $_POST['password'];
+    } else {
+        $errors[] = 'passwordが不正';
+    }
+
+
+    if(empty($errors)) {
+        $sql = 'SELECT * FROM users WHERE userId = :userId';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':userId', $_POST['id'], \PDO::PARAM_INT);
+        $stmt->execute();
+
+        if(empty($stmt->fetch())){
+            $sql = 'INSERT INTO users (userId, name, password) VALUES (:userId, :name, :password)';
             $stmt = $pdo->prepare($sql);
             $stmt->bindValue(':userId', $_POST['id'], \PDO::PARAM_INT);
+            $stmt->bindValue(':name', $_POST['name'], \PDO::PARAM_STR);
+            $stmt->bindValue(':password', $_POST['password'], \PDO::PARAM_STR);
             $stmt->execute();
+
             var_dump($stmt->fetch());
-            echo '2';
-            if(empty($stmt->fetch())){
-                echo '3';
-                $sql = 'INSERT INTO users (userId, name, password) VALUES (:userId, :name, :password)';
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindValue(':userId', $_POST['id'], \PDO::PARAM_INT);
-                $stmt->bindValue(':name', $_POST['name'], \PDO::PARAM_STR);
-                $stmt->bindValue(':password', $_POST['password'], \PDO::PARAM_STR);
-                $stmt->execute();
-
-                var_dump($stmt->fetch());
-            } else {
-                echo 'そのIDはすでに使われています。';
-            }
-
         } else {
-            echo 'IDは半角数字で入力してください';
+            echo 'そのIDはすでに使われています。';
         }
     } else {
         echo '全ての値を入力してください';
     }
-
-
-
 
 //    header('Location: index.html');
 } catch (Exception $e) {
