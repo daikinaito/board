b5e36c063fd6a841909a5192d89fe38de9ed8314
GONBE <mrgonbegonbe@gmail.com>
2018-11-17 11:21:15 +0900

    エラー文

--- board2/register.php
+++ board2/register.php
@@ -9,28 +9,32 @@ try {
     $name = $_POST['name'];
     $password = $_POST['password'];
 
-    if (is_int($id)) {
-        $sql = 'SELECT * FROM users WHERE userId = :userId';
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':userId', $_POST['id'], \PDO::PARAM_INT);
-        $exist = $stmt->execute();
-
-        if(empty($exist)){
-
-            $sql = 'INSERT INTO users (userId, name, password) VALUES (:userId, :name, :password)';
+    if(isset($id) and isset($name) and isset($password)){
+        if (is_int($id)) {
+            $sql = 'SELECT * FROM users WHERE userId = :userId';
             $stmt = $pdo->prepare($sql);
             $stmt->bindValue(':userId', $_POST['id'], \PDO::PARAM_INT);
-            $stmt->bindValue(':name', $_POST['name'], \PDO::PARAM_STR);
-            $stmt->bindValue(':password', $_POST['password'], \PDO::PARAM_STR);
-            $hoge = $stmt->execute();
+            $exist = $stmt->execute();
 
-            var_dump($hoge);
+            if(empty($exist)){
+
+                $sql = 'INSERT INTO users (userId, name, password) VALUES (:userId, :name, :password)';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':userId', $_POST['id'], \PDO::PARAM_INT);
+                $stmt->bindValue(':name', $_POST['name'], \PDO::PARAM_STR);
+                $stmt->bindValue(':password', $_POST['password'], \PDO::PARAM_STR);
+                $hoge = $stmt->execute();
+
+                var_dump($hoge);
+            } else {
+                echo 'そのIDはすでに使われています。';
+            }
+
         } else {
-            echo 'そのIDはすでに使われています。';
+            echo 'IDは半角数字で入力してください';
         }
-
     } else {
-        echo 'IDは半角数字で入力してください';
+        echo '全ての値を入力してください';
     }
 
 
