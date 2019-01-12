c22a095bcf147fe24a7661b8a0a0323910f51bcb
daikinaito <38032653+daikinaito@users.noreply.github.com>
2019-01-12 15:25:03 +0900

    Delete database_conf.php

--- database_conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-$DBSERVER = 'localhost';
-$DBNAME = 'board';
-$DBUSER = 'gonbe'; //作成したユーザー名
-$DBPASSWD = 'apple'; //作成したユーザーのパスワード
-$dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8;";
